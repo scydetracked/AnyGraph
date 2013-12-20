2492af1adeb70a94bcb384331e2de498718d4b97
Phillipe Côté <phillipe.cote@gmail.com>
2013-12-20 13:56:52 -0500

    - Initial Commit

---
+++ Editor/AnyGraphInspector.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(IAnyGraphable), true)]
+public class AnyGraphInspector : Editor {
+	public override void OnInspectorGUI (){
+		if(target is IAnyGraphable){
+			if(GUILayout.Button ("View In AnyGraph")){
+				EditorWindow.GetWindow<AnyGraph>();
+			}
+		}
+		//base.OnInspectorGUI ();
+	}
+}
---
+++ IAnyGraphable.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Interface to attach on a class to make it graphable.
+/// </summary>
+public interface IAnyGraphable{
+	/// <summary>
+	/// The GraphSettings used by the editor(Should be linked to a field in the class implementing it for serialization to work).
+	/// </summary>
+	/// <value>The graph settings.</value>
+	AnyGraphSettings Settings{get; set;}
+
+	/// <summary>
+	/// List of nodes that get called for drawing.
+	/// </summary>
+	List<IAnyGraphNode> Nodes{get;}
+	
+	/// <summary>
+	/// Function called to draw a node.
+	/// </summary>
+	/// <returns>
+	/// Return true if the gui needs repainting.
+	/// </returns>
+	/// <param name='n'>Node to draw.</param>
+	bool DrawNode(IAnyGraphNode n);
+	
+	/// <summary>
+	/// Any code in this will be drawn by the editor window.
+	/// </summary>
+	void AdditionalOptionsGUI();
+	
+	/// <summary>
+	/// Called by AnyGraph when connecting two nodes.
+	/// </summary>
+	/// <param name='n1'>Source Node.</param>
+	/// <param name='n2'>Target Node.</param>
+	void ConnectNodes(UnityEngine.Object n1, UnityEngine.Object n2);
+
+	/// <summary>
+	/// Called by AnyGraph when disconnecting two nodes.
+	/// </summary>
+	/// <param name="n1">Node 1.</param>
+	/// <param name="n2">Node 2.</param>
+	void DisconnectNodes(UnityEngine.Object n1, UnityEngine.Object n2);
+}
